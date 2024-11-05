a68641f841ef8c1f4f03e49873d5d065a6db2f7c
Raines <rainesg1996@gmail.com>
2024-11-05 16:06:52 +0800

    ci(publish): release before publish

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { Button } from './components/button';
-export type { ButtonType } from './components/button';
-export { ImageList } from './components/imageList';
-export type { ImageListType } from './components/imageList';

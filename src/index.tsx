63f99366d330f06f97bc24108d9cd9501d4d293f
Raines <rainesg1996@gmail.com>
2024-11-07 10:30:03 +0800

    chore(eslint): update eslint version

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,5 @@ export { Button } from './components/button';
 export type { ButtonType } from './components/button';
 export { ImageList } from './components/imageList'
 export type { ImageListType, ImageItemType } from './components/imageList'
+export { Upload } from './components/upload'
+export type { UploadType } from './components/upload'
